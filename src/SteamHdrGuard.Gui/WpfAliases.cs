b2c9049ad9d8b67a66128bbe3ab07f8d454f4789
Amamiyaren <70103741+renyumeng1@users.noreply.github.com>
2026-07-05 19:02:40 +0800

    Add WPF Application alias to avoid WinForms ambiguity

--- src/SteamHdrGuard.Gui/WpfAliases.cs
+++ src/SteamHdrGuard.Gui/WpfAliases.cs
@@ -1,2 +1,3 @@
+global using Application = System.Windows.Application;
 global using ComboBox = System.Windows.Controls.ComboBox;
 global using SelectionChangedEventArgs = System.Windows.Controls.SelectionChangedEventArgs;
