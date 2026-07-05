657498b04435e7fc1161d138de7dc6e243c4a889
Amamiyaren <70103741+renyumeng1@users.noreply.github.com>
2026-07-05 11:48:02 +0800

    Remove temporary native smoke test

--- src/SteamHdrGuard.Core/NativeSmokeTest.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace SteamHdrGuard.Core;
-
-internal static class NativeSmokeTest
-{
-    [DllImport("user32.dll")]
-    public static extern int GetSystemMetrics(int nIndex);
-}
